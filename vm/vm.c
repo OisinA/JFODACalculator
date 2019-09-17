2a2469f98bbf756163371660a2d17b400bf209e6
Arthan Simeon Jansen <asj2@csgate.ucc.ie>
2019-09-17 13:54:26 +0100

    added isa and vm changes

--- vm/vm.c
+++ vm/vm.c
@@ -5,10 +5,17 @@ int main(int argc, char* argv[]) {
   FILE* fp = fopen(argv[1], "r");  // Open file name specified in command line
   fseek(fp, 0, SEEK_END);          // Seek to the end of the file
   int size = ftell(fp);  // Store the current location as the size of the file
-  Instruction instructions[size / sizeof(Instruction)];  // Create an array of
+  char instructions[size];  // Create an array of
                                                          // Instructions
   rewind(fp);                        // Rewind to the start of the file
   fread(instructions, size, 1, fp);  // Read the binary from the file
+  for (int i = 0; i < size; i++) {
+      switch(instructions[i]){
+          case PUSHINT:
+            
+            break;
+      }
+  }
 
   return 0;
 }
