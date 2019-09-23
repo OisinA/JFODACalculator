cfc62e1334adf6b720f106d4c1c49e48a5b2611f
Arthan Simeon Jansen <asj2@csg21-15.ucc.ie>
2019-09-23 16:09:27 +0100

    Abstractions and fixed converting between int and float for storage

--- vm/vm.c
+++ vm/vm.c
@@ -3,13 +3,66 @@
 #include "../isa/isa.h"
 #include "stack.h"
 
-struct pushint {
-  int data;
-};
+StackNode* pushValue(int type, char* instructions, int i, int size, StackNode* stack) {
+  printf("%d:%d%c", i, size/sizeof(char), 10);
+  if (size - (sizeof(char) * (i + 1)) < sizeof(int)) {
+    printf("Error, No data after PUSH instruction");
+    exit(1);
+  }
+  printf("Loaded %d", *(int*)(instructions + i + 1));
+  Data data = {type, *(int*)(instructions + i + 1)};
 
-struct pushfloat {
-  float data;
-};
+  push(&stack, data);
+  return stack;
+}
+
+StackNode* operate(int operator, StackNode* stack) {
+  if (isEmpty(stack)) {
+    printf("Error, Not enough items in stack for operation");
+    exit(1);
+  }
+  Data data1 = pop(&stack);
+  if (isEmpty(stack)) {
+    printf("Error, Not enough items in stack for operation");
+    exit(1);
+  }
+  Data data2 = pop(&stack);
+  if (data1.type != data2.type) {
+    printf("%d %d%c", data1.type, data2.type, 10);
+    printf("Error, cannot operate on Float with int");
+    exit(1);
+  }
+  Data data = {data1.type, 0};
+  if (data1.type == 1) {
+    data.value = 1;
+    data1.value = *(float*)&data1.value;
+    data2.value = *(float*)&data2.value;
+  }
+  switch (operator) {
+    case ADD:
+      data.value = data1.value + data2.value;
+      break;
+    case SUB:
+      data.value = data1.value - data2.value;
+      break;
+    case MUL:
+      data.value = data1.value * data2.value;
+      break;
+    case EXP:
+      data.value = pow(data1.value, data2.value);
+      break;
+    case DIV:
+      data.value = data1.value / data2.value;
+      break;
+    default:
+      printf("Error, unrecognized instruction");
+      exit(1);
+  }
+  if (data1.type == 1) {
+  }
+  push(&stack, data);
+  return stack;
+}
 
 int main(int argc, char* argv[]) {
   FILE* fp = fopen(argv[1], "r");  // Open file name specified in command line
@@ -21,158 +74,18 @@ int main(int argc, char* argv[]) {
   fread(instructions, size, 1, fp);  // Read the binary from the file
   StackNode* stack = NULL;
   for (int i = 0; i < size; i++) {
-    Data data = {0, 0};
-    Data data1 = {0, 0};
-    Data data2 = {0, 0};
+    printf("instruction:%x%c", instructions[i], 10);
     switch (instructions[i]) {
       case PUSHINT:
-        if (sizeof(instructions) - (sizeof(char) * (i + 1)) < sizeof(long)) {
-          printf("Error, No data after PUSHINT instruction");
-          return 1;
-        }
-        data.value = *(long*)(instructions + i + 1);
-        push(&stack, data);
-        i += 8;
+        stack = pushValue(0, instructions, i, sizeof(instructions), stack);
+        i += sizeof(int);
         break;
       case PUSHFLOAT:
-        if (sizeof(instructions) - (sizeof(char) * (i + 1)) < sizeof(long)) {
-          printf("Error, No data after PUSHFLOAT instruction");
-          return 1;
-        }
-        printf("%f", (*(float*)(instructions + i + 1)));
-        data.value = (*(float*)(instructions + i + 1));
-        data.type = 1;
-        push(&stack, data);
-        i += 8;
-        break;
-      case ADD:
-        if (isEmpty(stack)) {
-          printf("Error, Not enough items in stack for ADD");
-          return 1;
-        }
-        data1 = pop(&stack);
-        if (isEmpty(stack)) {
-          printf("Error, Not enough items in stack for ADD");
-          return 1;
-        }
-        data2 = pop(&stack);
-        if (data1.type != data2.type) {
-          printf("Error, cannot ADD Float with Long");
-          return 1;
-        }
-        if (data1.type == 0) {
-          long result = data1.value + data2.value;
-          Data data = {0, result};
-          push(&stack, data);
-        } else {
-          float result = (float)(data1.value) + (float)(data2.value);
-          Data data = {1, result};
-          push(&stack, data);
-        }
-        break;
-      case SUB:
-        if (isEmpty(stack)) {
-          printf("Error, Not enough items in stack for SUB");
-          return 1;
-        }
-        data1 = pop(&stack);
-        if (isEmpty(stack)) {
-          printf("Error, Not enough items in stack for SUB");
-          return 1;
-        }
-        data2 = pop(&stack);
-        if (data1.type != data2.type) {
-          printf("Error, cannot SUB Float with Long");
-          return 1;
-        }
-        if (data1.type == 0) {
-          long result = data1.value - data2.value;
-          Data data = {0, result};
-          push(&stack, data);
-        } else {
-          float result = (float)(data1.value) - (float)(data2.value);
-          Data data = {1, result};
-          push(&stack, data);
-        }
-        break;
-      case MUL:
-        if (isEmpty(stack)) {
-          printf("Error, Not enough items in stack for MUL");
-          return 1;
-        }
-        data1 = pop(&stack);
-        if (isEmpty(stack)) {
-          printf("Error, Not enough items in stack for MUL");
-          return 1;
-        }
-        data2 = pop(&stack);
-        if (data1.type != data2.type) {
-          printf("Error, cannot MUL Float with Long");
-          return 1;
-        }
-        if (data1.type == 0) {
-          long result = data1.value * data2.value;
-          Data data = {0, result};
-          push(&stack, data);
-        } else {
-          float result = (float)(data1.value) * (float)(data2.value);
-          Data data = {1, result};
-          push(&stack, data);
-        }
-        break;
-      case DIV:
-        if (isEmpty(stack)) {
-          printf("Error, Not enough items in stack for DIV");
-          return 1;
-        }
-        data1 = pop(&stack);
-        if (isEmpty(stack)) {
-          printf("Error, Not enough items in stack for DIV");
-          return 1;
-        }
-        data2 = pop(&stack);
-        if (data1.type != data2.type) {
-          printf("Error, cannot DIV Float with Long");
-          return 1;
-        }
-        if (data1.type == 0) {
-          long result = data1.value / data2.value;
-          Data data = {0, result};
-          push(&stack, data);
-        } else {
-          float result = (float)(data1.value) / (float)(data2.value);
-          Data data = {1, result};
-          push(&stack, data);
-        }
-        break;
-      case EXP:
-        if (isEmpty(stack)) {
-          printf("Error, Not enough items in stack for EXP");
-          return 1;
-        }
-        data1 = pop(&stack);
-        if (isEmpty(stack)) {
-          printf("Error, Not enough items in stack for EXP");
-          return 1;
-        }
-        data2 = pop(&stack);
-        if (data1.type != data2.type) {
-          printf("Error, cannot EXP Float with Long");
-          return 1;
-        }
-        if (data1.type == 0) {
-          long result = pow(data1.value, data2.value);
-          Data data = {0, result};
-          push(&stack, data);
-        } else {
-          float result = pow((float)(data1.value), (float)(data2.value));
-          Data data = {1, result};
-          push(&stack, data);
-        }
+        stack = pushValue(1, instructions, i, sizeof(instructions), stack);
+        i += sizeof(int);
         break;
       default:
-        printf("Error, Unrecognized instruction");
-        return 1;
+        stack = operate(instructions[i], stack);
     }
   }
 
@@ -180,7 +93,7 @@ int main(int argc, char* argv[]) {
   if (data.type == 0) {
     printf("%ld", data.value);
   } else {
-    printf("%f", (float)data.value);
+    printf("%f", *(float*)&data.value);
   }
 
   return 0;
