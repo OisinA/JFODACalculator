17c5d3409530fc3cc119d0451efb64cfefb23b3f
OisinA <oisinaylward@gmail.com>
2019-09-24 19:21:14 +0100

    Changes, not sure what tbh

--- generator/main.c
+++ generator/main.c
@@ -4,11 +4,12 @@
 #include "../isa/isa.h"
 
 void print(char* string);
-void emit(char byte);
+void _emit(char byte);
+void emit(char byte[], int size);
 int toInt(char* string);
 float toFloat(char* string);
 
-char output[5] = {};
+char output[128] = {};
 int outputIndex = 0;
 
 int main() {
@@ -25,42 +26,55 @@ int main() {
 
     // fclose(fptr);
 
-    Token t = {"40", INTEGER};
-    Token t1 = {"10", INTEGER};
-    Token t2 = {"+", OPERATOR};
-    Token tokens[] = {t, t1, t2};
+    remove("output");
+
+    Token t = {"40.5", FLOAT};
+    Token t1 = {"10.2", FLOAT};
+    Token t2 = {"*", OPERATOR};
+    Token t3 = {"5.0", FLOAT};
+    Token t4 = {"/", OPERATOR};
+    Token tokens[] = {t, t1, t2, t3, t4};
 
     // Loop through each token, one by one.
-    for(int i = 0; i < sizeof(tokens) / sizeof(tokens[0]); i++) {
+    for(int i = sizeof(tokens) / sizeof(tokens[0]); i >= 0; i--) {
         // Switch for each token type, emitting the instruction.
         switch(tokens[i].tokenType) {
             case INTEGER:
                 printf("it was a integer: %s%c", tokens[i].val, 10);
-                emit(PUSHINT);
-                emit(toInt(tokens[i].val));
+                int n = toInt(tokens[i].val);
+                emit((char*)&n, 4);
+                emit((char[]){PUSHINT}, 1);
                 break;
             case FLOAT:
                 printf("it was a float: %s%c", tokens[i].val, 10);
-                emit(PUSHFLOAT);
-                emit(toFloat(tokens[i].val));
+                float f = toFloat(tokens[i].val);
+                emit((char*)&f, 4);
+                emit((char[]){PUSHFLOAT}, 1);
                 break;
             case OPERATOR:
                 printf("it was an operator: %s%c", tokens[i].val, 10);
                 if(strcmp(tokens[i].val, "+") == 0) {
-                    emit(ADD);
+                    emit((char[]){ADD}, 1);
+                    break;
+                }
+                if(strcmp(tokens[i].val, "-") == 0) {
+                    emit((char[]){SUB}, 1);
+                    break;
+                }
+                if(strcmp(tokens[i].val, "/") == 0) {
+                    emit((char[]){DIV}, 1);
+                    break;
+                }
+                if(strcmp(tokens[i].val, "*") == 0) {
+                    emit((char[]){MUL}, 1);
+                    break;
+                }
+                if(strcmp(tokens[i].val, "**") == 0) {
+                    emit((char[]){EXP}, 1);
+                    break;
                 }
                 break;
-            case LPAREN:
-                print("it was a left parenthesis");
-                break;
-            case RPAREN:
-                print("it was a right parenthesis");
-                break;
         }
-    }
-
-    for(int byteIndex = 0; byteIndex < sizeof(output) / sizeof(output[0]); byteIndex++) {
-        printf("%x%c", output[byteIndex], 10);
     }
 
     return 0;
@@ -70,9 +84,20 @@ void print(char* string) {
     printf("%s%c", string, 10);
 }
 
-void emit(char byte) {
+void _emit(char byte) {
     output[outputIndex] = byte;
     outputIndex = outputIndex + 1;
+}
+
+void emit(char byte[], int size) {
+    FILE* file = fopen("output", "ab");
+    fwrite(byte, 1, size, file);
+    for(int byteIndex = 0; byteIndex < size * sizeof(char); byteIndex++) {
+        // if(output[byteIndex] == 0) {
+        //     continue;
+        // }
+        printf("%hhx%c", byte[byteIndex], 10);
+    }
 }
 
 int toInt(char* string) {
